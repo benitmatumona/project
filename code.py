16102c106072be01e425b23b834304230405fea6
benit <polviedivine@gmail.com>
2026-06-02 00:14:56 +0200

    success

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 gas = pd.read_csv("data/gas_prices.csv")
 
 
-def gas_price(*countries: str)-> bool:
+def gas_price(*countries: str)-> plt:
     for country in countries:
         try:
             plt.plot(gas.Year, gas[country], label=country)
@@ -14,12 +14,21 @@ def gas_price(*countries: str)-> bool:
             print(f"invalid country name or format {country}")
         
         
-    plt.title("Gas Price Over Time")
+    plt.title("Gas Price Over Time", fontdict={"fontsize": "20", "fontweight": "bold", })
     plt.xlabel("Year")
     plt.ylabel("Price(USD)")
     plt.xticks(gas.Year[::3])
-    plt.show()
-    return True
+    return plt
     
-    
-gas_price("USA", "South Korea", "Canada", "China")
+
+def save_plt(plt: plt, location: str)-> bool:
+    try:
+        plt.savefig(location, dpi=300)
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+
+
+graph = gas_price("USA", "South Korea", "Canada", "China")
+graph.show()
