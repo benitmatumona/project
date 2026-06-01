d31efa84d91d63c6e5b2cc1d5e67fc0758c4cbcd
benit <polviedivine@gmail.com>
2026-06-02 00:35:51 +0200

    success

--- data_fuctions.py
+++ data_fuctions.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 
 
 gas = pd.read_csv("data/gas_prices.csv")
+fifa = pd.read_csv("data/fifa_data.csv")
 
 
 def gas_price(*countries: str)-> plt:
+    """creates a plot according to any contry/countries specified"""
     for country in countries:
         try:
             plt.plot(gas.Year, gas[country], label=country)
@@ -14,7 +16,10 @@ def gas_price(*countries: str)-> plt:
             print(f"invalid country name or format {country}")
         
         
-    plt.title("Gas Price Over Time", fontdict={"fontsize": "20", "fontweight": "bold", })
+    plt.title(
+        "Gas Price Over Time", 
+        fontdict={"fontsize": "20", "fontweight": "bold", }
+        )
     plt.xlabel("Year")
     plt.ylabel("Price(USD)")
     plt.xticks(gas.Year[::3])
@@ -22,6 +27,7 @@ def gas_price(*countries: str)-> plt:
     
 
 def save_plt(plt: plt, location: str)-> bool:
+    """saves the file at a desired location according to the second argument"""
     try:
         plt.savefig(location, dpi=300)
     except Exception as e:
@@ -29,6 +35,11 @@ def save_plt(plt: plt, location: str)-> bool:
         return False
 
 
-
-graph = gas_price("USA", "South Korea", "Canada", "China")
-graph.show()
+def fifa_hist()-> bool:
+    bins = [40, 50, 60, 70, 80, 90, 100]
+    plt.hist(fifa.Overoll, bins= bins)
+    plt.title("Distribution Of Players According To Their Skills")
+    plt.xticks(bins)
+    plt.xlabel("Skill Level")
+    plt.ylabel("number of players")
+    return True
